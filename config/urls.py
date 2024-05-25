8dc910e16b829697e398d554bde5ac15149da71b
ClassiCcOwl <khavari.7878@yahoo.com>
2024-05-26 00:57:08 +0330

    fix: home page is swagger now

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path(
-        "swagger/",
+        "",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
@@ -28,10 +28,6 @@ urlpatterns = [
         "v1/status/",
         StatusView.as_view(),
         name="Status",
-    ),
-    path(
-        "redoc/",
-        schema_view.with_ui("redoc", cache_timeout=0),
     ),
     path(
         "admin/",
